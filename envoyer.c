44078450e61d81e32c0c9dd7d91f108e96336f92
AlperROzkan <alper.ozkan.p@gmail.com>
2020-02-04 17:33:02 +0100

    envoyer and recevoir done

--- envoyer.c
+++ envoyer.c
@@ -1,6 +1,15 @@
-#include <sys/types.h>
+#include <sys/unistd.h>
+
+#include "signatures.h"
 
 int envoyer(int prise, char *buffer, size_t taille)
 {
-
+    if (write(prise, buffer, taille)== taille)
+    {
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
 }
--- recevoir.c
+++ recevoir.c
@@ -1,6 +1,15 @@
-#include <sys/types.h>
+#include <sys/unistd.h>
+#include "signatures.h"
 
 int recevoir(int prise, char *buffer, size_t taille)
 {
-
+    if (read(prise, buffer, taille) >= 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+    
 }
